afd5ab2c1f5bb17834aaf1832008b052435b849e
Mayank-Admin <mayank-admin@MacBook-Pro-von-Mayank.local>
2024-02-22 16:22:20 +0100

    Active player state change

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,18 +1,35 @@
+import { useState } from "react";
+
 const initialGameboard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
+  const [gameBoard, setGameBoard] = useState(initialGameboard);
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedGameBoard = [
+        ...prevGameBoard.map((innerArray) => [...innerArray]),
+      ];
+      updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
+      return updatedGameBoard;
+    });
+
+    onSelectSquare();
+  }
   return (
     <ol id="game-board">
-      {initialGameboard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [isEditing, setIsEditing] = useState(false);
 
   const [playerName, setPlayerName] = useState(initialName || "");
@@ -20,7 +20,7 @@ const Player = ({ initialName, symbol }) => {
     );
   }
   return (
-    <li>
+    <li className={isActive ? "active" : undefined}>
       <span>
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
